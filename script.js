d3efd8d822e34e86d5c7f6710579f943cca15db5
Robert Teets <teetsrobert@gmail.com>
2022-07-23 18:14:21 -0600

    syncing

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ hamburgerToggle.addEventListener('click', function () {
 
     if (hamburgerToggle.checked) {
         $('.sidebar').css("width", "300px")
+        $('#overlay').css("visibility", "visible")
     }
     else {
         $('.sidebar').css("width", "0px")
+        $('#overlay').css("visibility", "hidden")
     }
 })
