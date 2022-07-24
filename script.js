6bd2e69b0dd53d293d4dffb2391c1988a1a90935
Robert Teets <teetsrobert@gmail.com>
2022-07-23 18:54:08 -0600

    added placeholder content and fixed responsiveness

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let hamburgerToggle = document.querySelector('#hamburger-toggle');
 
 hamburgerToggle.addEventListener('click', function () {
-
     if (hamburgerToggle.checked) {
         $('.sidebar').css("width", "300px")
         $('#overlay').css("visibility", "visible")
+        $('#overlay').css("opacity", "0.5");
     }
     else {
         $('.sidebar').css("width", "0px")
-        $('#overlay').css("visibility", "hidden")
+        $('#overlay').css("opacity", "0");
+        setTimeout(function () {
+            $('#overlay').css("visibility", "hidden")
+        }, 200)
     }
 })
+
+console.log("Code Loaded...")
